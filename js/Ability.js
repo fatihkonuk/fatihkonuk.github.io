49865396096e00dfa0d5a7526e20564df4b8f24c
Fatih Konuk <fatihkonuk000@gmail.com>
2023-04-28 20:51:14 +0300

    something changed

--- js/Ability.js
+++ js/Ability.js
@@ -11,6 +11,13 @@ class Ability {
 const ability_list = [
     new Ability("HTML",90),
     new Ability("CSS",75),
+    new Ability("BOOTSTRAP",60),
     new Ability("JAVASCRIPT",50),
-    new Ability("BOOTSTRAP",60)
+    new Ability("JQuery",70),
+    new Ability("Node.js",80),
+    new Ability("Mongodb",40),
+    new Ability("C#",50),
+    new Ability("Asp.Net",30),
+    new Ability("SQL",30),
+
 ]
